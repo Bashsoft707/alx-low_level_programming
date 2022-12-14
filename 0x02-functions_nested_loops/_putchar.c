44e97d951735818b4ec2c1f64615ccb45390b89f
Bashsoft707 <alausabashir@gmail.com>
2022-12-14 06:11:17 -0800

    put char

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -4,7 +4,7 @@
  * _putchar - function that returns a character
  * @c: The character to print
  * Description: function uses putchar to return character
- * Return value of '_putchar'
+ * Return: value of '_putchar'
  */
 
 int _putchar(char c)
