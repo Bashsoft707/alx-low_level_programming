c19011f8c41893ad7b2eb5f5b6a4fd8e51e0bcdb
Bashsoft707 <alausabashir@gmail.com>
2022-12-14 17:19:07 -0800

    done

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,51 +9,43 @@
 
 void print_times_table(int n)
 {
-	int i, j;
+	int i = 0, j, k;
 
-	if (n > 0 && n < 15)
+	if (n > 0 || n < 15)
+		return;
+	while (x <= n)
 	{
-		for (i = 0; i <= n; i++)
+		for (j = 0; j <= n; j++)
 		{
-			_putchar('0');
+			k = x * y;
+			if (k > 99)
+			{
+				_putchar(k / 100 + '0');
+				_putchar((k / 10 % 10) + '0');
+				_putchar(k % 10 + '0');
+			}
+			else if (k > 9)
+			{
+				_putchar(' ');
+				_putchar(k / 10 + '0');
+				_putchar(k % 10 + '0');
+			}
+			else if (j != 0)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(k + '0');
+			}
+			else
+				_putchar(k + '0');
 
-			for (j = 1; j <= n; j++)
-				putformat(i * j);
-			_putchar('\n');
+			if (j != n)
+			{
+				_putchar(', ');
+				_putchar(' ');
+			}
 		}
-	}
-}
-
-/**
- * putformat - Format characters to output
- * @n: An integer n
- * Return: Nothing
- */
-
-void putformat(int n)
-{
-	if (n <= 9)
-	{
-		_putchar(', ');
-		_putchar(' ');
-		_putchar(' ');
-		_putchar(' ');
-		_putchar(n + '0');
-	}
-	esle if (n > 9 && n <= 99)
-	{
-		_putchar(', ');
-		_putchar(' ');
-		_putchar(' ');
-		_putchar((n / 10) + '0');
-		_putchar((n % 10) + '0');
-	}
-	else
-	{
-		_putchar(', ');
-		_putchar(' ');
-		_putchar(n / 100 + '0');
-		_putchar(n / 10 % 10 + '0');
-		_putchar(n % 10 + '0');
+		_putchar('\n');
+		i++
 	}
 }
