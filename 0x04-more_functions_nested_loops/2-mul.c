e65ba66974978ae101440b63595d08780dc07d9f
Bashsoft707 <alausabashir@gmail.com>
2022-12-18 05:23:55 -0800

    multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiply 2 integers, x and y
+ * @x: integer x
+ * @y: integer y
+ * Return: their multiplication
+ */
+
+int mul(int x, int y)
+{
+	return (x * y);
+}
