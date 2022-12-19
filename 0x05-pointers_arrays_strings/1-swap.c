2f4e59e26ccd3c5830e84629da5e9c0d523b2c24
Bashsoft707 <alausabashir@gmail.com>
2022-12-19 02:46:15 -0800

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap - function that swaps the value of two integers
+ * @x: Integer x
+ * @y: Integer y
+ * Return: Nothing
+ */
+
+void swap(int *x, int *y)
+{
+	int tmp;
+
+	tpm = *x;
+	*x = *y;
+	*y = tmp;
+}
