417a56fbdb200a98d535e78e7d49b529421cc06b
Bashsoft707 <alausabashir@gmail.com>
2023-01-04 15:00:02 -0800

    pliandrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -14,30 +14,30 @@ int _strlen_recursion(char *s)
 }
 
 /**
- * palidrome_finder - find palidrome
+ * palindrome_finder - find palidrome
  * @s: string s
  * @l: input l
  * Return: return 1 if palidrom, otherwise 0
  */
 
-int palidrome_finder(char *s, int l)
+int palindrome_finder(char *s, int l)
 {
 	if (l < 1)
 		return (1);
 	if (*s == *(s + 1))
-		return (palidrome_finder(s + 1, l - 2));
+		return (palindrome_finder(s + 1, l - 2));
 	return (0);
 }
 
 /**
- * is_palidrome - return if string is palidrome or not
+ * is_palindrome - return if string is palidrome or not
  * @s: input string
  * Return: 1 or 0
  */
 
-int is_palidrome(char *s)
+int is_palindrome(char *s)
 {
 	int len = _strlen_recursion(s);
 
-	return (palidrome_finder(s, len - 1));
+	return (palindrome_finder(s, len - 1));
 }
