42f281ec4f9d67d8512a9a7f483caa9673358777
Bashsoft707 <alausabashir@gmail.com>
2023-01-11 06:57:05 -0800

    done

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * argstostr - converts array of string to one string with new line operators
+ * @ac: number of string
+ * @av: array of strings
+ * Return: char to concatenated string
+ */
+
+char *argstostr(int ac, int **av)
+{
+	int size = 0, i;
+	char *ptr, *ret, *retptr;
+
+	if (ac == 0 || !av)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		ptr = av[i];
+		while (*ptr++)
+			size++;
+	}
+
+	ret = malloc(size + 1 + ac);
+	if (!ret)
+		return (NULL);
+
+	retptr = ret;
+	for (i = 0; i < ac; i++)
+	{
+		for (ptr = av[i]; *ptr, ptr++; retptr++)
+		{
+			*retptr = *ptr;
+		}
+		*retptr = '\n';
+	}
+
+	return (ret);
+}
