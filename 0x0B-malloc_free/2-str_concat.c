148b64746c8f8a1ee898ccf4f04baa4d99a3af4d
Bashsoft707 <alausabashir@gmail.com>
2023-01-11 06:15:39 -0800

    done

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -19,14 +19,18 @@ char *str_concat(char *s1, char *s2)
 	if (s1 == NULL)
 		s1 = "";
 	while (*s1)
+	{
 		lens1++;
 		s1++;
+	}
 	s1 = start1;
 	if (s2 == NULL)
 		s2 = "";
 	while (*s2)
+	{
 		lens2++;
 		s2++;
+	}
 	s2 = start2;
 	new_str = malloc(sizeof(char) * (lens1 + lens2 + 1));
 	start1 = new_str;
@@ -35,11 +39,15 @@ char *str_concat(char *s1, char *s2)
 	for (; i < (lens1 + lens2); i++)
 	{
 		if (i < lens1)
+		{
 			new_str[i] = *s1;
 			s1++;
+		}
 		else
+		{
 			new_str[i] = *s2;
 			s2++;
+		}
 	}
 	new_str[i] = '\0';
 	return (start1);
