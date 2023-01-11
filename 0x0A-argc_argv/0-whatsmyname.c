096cc2f17a4e7b73818125fcccf3db26f59a5159
Bashsoft707 <alausabashir@gmail.com>
2023-01-11 14:21:36 -0800

    done

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the program
+ * @argc: number of arguents to provide
+ * @argv: argument to perform
+ * Return: 0 if successful
+ */
+
+int main(int argc, char **argv)
+{
+	printf("%s\n", argv[argc - argc]);
+	return (0);
+}
