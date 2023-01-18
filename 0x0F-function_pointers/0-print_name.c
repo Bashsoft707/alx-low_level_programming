c6d44a0f66e5f09b0c0642f677a9dca38b4e0951
Bashsoft707 <alausabashir@gmail.com>
2023-01-18 15:46:20 -0800

    done

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * print_name - function that prints a name
+ * @name: name to print
+ * @f: function to use
+ * Return: Nothing
+ */
+
+void print_name(char *name, void (*f)(char *name))
+{
+	if (f != NULL && name != NULL)
+		(*f)(name);
+}
