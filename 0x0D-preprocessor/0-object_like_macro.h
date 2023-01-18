70cb8cfaeb20e6386250ebb2c9e69b2b346f5c0c
Bashsoft707 <alausabashir@gmail.com>
2023-01-17 16:06:58 -0800

    done

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,7 +1,4 @@
-#ifndef OBJECTLIKEH
-#define OBJECTLIKEH
+#ifndef _OBJECT_LIKE_MACRO_H_
+#define _OBJECT_LIKE_MACRO_H_
 #define SIZE 1024
-
-typedef int asdf
-
-#endif
+#endif /* _OBJECT_LIKE_MACRO_H_ */
