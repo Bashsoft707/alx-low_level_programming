7965c5db2a7c37a8e75735ab9ad76d942a38d4f6
Bashsoft707 <alausabashir@gmail.com>
2023-01-18 17:21:28 -0800

    done

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * int_index - check an array for an integer that matches
@@ -6,17 +6,25 @@
  * @array: array
  * @size: size of array
  * @cmp: comparison
- * Return index of match, otherwise -1
+ * Return: index of match, otherwise -1
  */
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (cmp != NULL && array != NULL)
-		for (i = 0; i < size; i++)
-			if (cmp(array[i]))
-				return (i);
+	if (cmp == NULL || array == NULL)
+		return (-1);
+
+	if (size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+		if (cmp(array[i]))
+			return (i);
+
+	if (i == size)
+		return (-1);
 
 	return (-1);
 }
