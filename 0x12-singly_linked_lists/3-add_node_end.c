24ad8be015437c34be7ae2cfbc73a675c60f9aa6
Bashsoft707 <alausabashir@gmail.com>
2023-01-26 14:33:46 -0800

    done

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -26,6 +26,11 @@ list_t *add_node_end(list_t **head, const char *str)
 	if (*head == NULL)
 	{
 		new_node->next = *head;
+		*head = new_node;
+	}
+	else
+	{
+		new_node->next = NULL;
 		temp = *head;
 		while (temp->next)
 			temp = temp->next;
