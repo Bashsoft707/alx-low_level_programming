8eb61dade057d51c7ecee9e24847dd0b94adff3e
Bashsoft707 <alausabashir@gmail.com>
2023-02-10 02:22:10 -0800

    done

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,7 +9,7 @@
  * Return: actual number of letters read
  */
 
-size_t read_textfile(const char *filename, size_t letters)
+ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int file;
 	int length, wrotechars;
