af500c741bef31c885f62b28bb46d55a10d4d386
Bashsoft707 <alausabashir@gmail.com>
2023-02-10 02:54:41 -0800

    done

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -4,7 +4,6 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/uio.h>
-#include <stdlib.h>
 
 /**
  * create_file - create a file and put text in it
@@ -15,25 +14,22 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int file;
-	int length, inlen = 0;
-	char *ptr;
+	int fdo, fdw, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	file = open(filename, O_WRONLY, O_CREAT, O_TRUNC, 0600);
-	if (file == -1)
+	fdo = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (fdo < 0)
 		return (-1);
 
-	if (text_content != NULL)
-	{
-		for (inlen = 0, ptr = text_content; *ptr, ptr++;)
-			inlen++;
-		length = write(file, text_content, inlen);
-	}
+	while (text_content && *(text_content + len))
+		len++;
 
-	if (close(file) == -1 || inlen != length)
+	fdw = write(fdo, text_content, len);
+	close(fdo);
+	if (fdw < 0)
 		return (-1);
+
 	return (1);
 }
